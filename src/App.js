1249fbcde31f4a926ba3a257140490ebc000077b
mfaishaladiyatma <mfaishall.adiyatma@gmail.com>
2022-06-02 16:18:00 +0700

    layout done but not responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Layout from './common/Layout';
+
+
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,35 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
 ],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        'sm': "600px",
+        'md': "700px",
+        'lg': "900px",
+        'xl': "1200px",
+        '2xl': "1400px",
+        '3xl': "1800px",
+      }
+    },
+    screens: {
+      sm: {'max': '640px'},
+      'md': {'min': '641px', 'max': '768px'},
+      'lg': {'min': '769px', 'max': '1024px'},
+      'xl': {'min': '1025px', 'max': '1280px'},
+      '2xl': {'min': '1281px', 'max': '1440px'},
+      '3xl': {'min': '1441px', 'max': '1920px'},
+    },
+    extend: {
+      colors: {
+        'blue-pale': '#6999B8',
+        'light-blue-pale': "#DBE4EA",
+
+      },
+      fontFamily: {
+        'helvetica': ['Helvetica', 'sans-serif'], //untuk custom font
+        Inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
