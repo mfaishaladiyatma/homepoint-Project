d7267b67534d79a970ca7f685c98212879b68cce
mfaishaladiyatma <mfaishall.adiyatma@gmail.com>
2022-06-07 21:39:58 +0700

    nambah addresProvider di Index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddressProvider from './context/context';
 
 import { BrowserRouter } from 'react-router-dom';
 
