b81fe5f7879622ecf617620260d0bb22415ac4ee
mfaishaladiyatma <mfaishall.adiyatma@gmail.com>
2022-06-12 14:55:17 +0700

    fixing navbar again and popup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,10 @@ import Bell from '../images/Bell.svg'
 import UserLogo from '../images/UserCircle.svg'
 import arrowDown from '../images/ChevronDown.svg'
 import { HiCheck } from "react-icons/hi";
+import searchLogo from '../images/search.svg'
 
 import { useState, Fragment } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
+import { useNavigate } from 'react-router-dom'
 
 const category = [
     {
@@ -39,113 +40,63 @@ const category = [
 
 export default function Header() {
     const [selected, setSelected] = useState('')
-    
+    const [isClickedLogin, setIsClickedLogin] = useState(false)
+    const [isClicked, setIsClicked] = useState(false)
+
+    const toggleSelected = (value) => {
+        if (selected) {
+            setSelected('')
+        } else {
+            setSelected(value)
+        }
+    }
+
+    const handleCategory = () => {
+        setIsClicked(!isClicked)
+    }
+
+    const navigate = useNavigate()
+
     console.log(selected)
+    console.log(isClicked)
     return (
-        <header>
+        <header className='sticky top-0 z-20'>
             <nav className='bg-blue-pale  h-20 flex items-center justify-center font-Inter'>
                 <div className='container px-3 flex items-center justify-between'>
                     <button>
                         <img src={CompanyLogo} alt="Homepoint" />
                     </button>
-                    <div className='bg-white w-[750px] h-[60px] p-2 flex gap-x-3 rounded-[10px]'>
+                    <div className='relative bg-white w-[750px] h-[60px] p-2 flex gap-x-3 rounded-[10px]'>
 
-                        <Listbox value={selected} onChange={setSelected}>
-                            <div className='relative h-full '>
-                                <Listbox.Button className='font-[400] px-2 text-left w-44 h-full  flex items-center justify-between'>
-                                    {selected ? <span>{selected}</span> : 'Kategori'}
-                                    {/* <span>{selected}</span> */}
-                                    <span><img src={arrowDown} alt="" /> </span>
-                                </Listbox.Button>
-                                <Transition
-                                    as={Fragment}
-                                    leave='transition ease-in duration-200'
-                                    leaveFrom='opacity-100'
-                                    leaveTo='opacity-0'
-                                >
-                                    <Listbox.Options className='w-44 rounded-[10px] border border-slate-400 absolute top-full mt-3 cursor-pointer z-10'>
-                                        <Listbox.Option
-                                            as={Fragment}
-                                            value={'Kursi'}>
-                                            {({ active, selected }) => (
-                                                <li className={`flex items-center px-2 py-2 gap-x-3 rounded-[10px] ${active ? 'bg-blue-300 ' : 'bg-white text-black'} ${selected ? 'font-bold' : 'font-normal'}`}>
-                                                    {selected ? (
-                                                        <HiCheck className='w-7 h-7 text-blue-500' />
-                                                    ) : null}
-                                                    {'Kursi'}
-                                                </li>
-                                            )}
-                                        </Listbox.Option>
-                                        <Listbox.Option
-                                            as={Fragment}
-                                            value={'Kursi 2'}>
-                                            {({ active, selected }) => (
-                                                <>
-                                                <li className={`flex items-center px-2 py-2 gap-x-3 rounded-[10px] ${active ? 'bg-blue-300 ' : 'bg-white text-black'} ${selected ? 'font-bold' : 'font-normal'}`}>
-                                                    {selected ? (
-                                                        <HiCheck className='w-7 h-7 text-blue-500' />
-                                                    ) : null}
-                                                    {'Kursi 2'}
-                                                </li>
-                                                <Listbox.Options
-                                                className={active ? 'block' : 'hidden'}>Test</Listbox.Options>
-                                                </>
-                                            )}
-                                        </Listbox.Option>
-                                        <Listbox.Option
-                                            as={Fragment}
-                                            value={'Kursi 3'}>
-                                            {({ active, selected }) => (
-                                                <li className={`flex items-center px-2 py-2 gap-x-3 rounded-[10px] ${active ? 'bg-blue-300 ' : 'bg-white text-black'} ${selected ? 'font-bold' : 'font-normal'}`}>
-                                                    {selected ? (
-                                                        <HiCheck className='w-7 h-7 text-blue-500' />
-                                                    ) : null}
-                                                    {'Kursi 3'}
-                                                </li>
-                                            )}
-                                        </Listbox.Option>
-                                        
-                                        {/* {category.map((item, index) => (
-                                            <Listbox.Option
-                                                key={index}
-                                                value={item}
-                                                as={Fragment}
-                                            >
-                                                {({ active, selected }) => (
-                                                    <li className={`flex items-center px-2 py-2 gap-x-3 rounded-[10px] ${active ? 'bg-blue-300 ' : 'bg-white text-black'} ${selected ? 'font-bold' : 'font-normal'}`}>
-                                                        {selected ? (
-                                                            <HiCheck className='w-7 h-7 text-blue-500' />
-                                                        ) : null}
-                                                        {item.name}
-                                                    </li>
-                                                )} */}
-                                        {/* className={({active}) =>
-                                            `${active ? 'bg-blue-300 text-blue-800' : 'text-black'}`
-                                            }>
-                                                {({selected}) => (
-                                                    <>
-                                                    <div className={`${selected ? 'font-bold' : 'font-normal'}`}>
-                                                        {item.name}
-                                                    </div>
-                                                    {selected ? (
-                                                        <div>
-                                                            <HiCheck className='text-blue-500' />
-                                                        </div>
-                                                    ) : null}
-                                                    </>
-                                                )} */}
-                                        {/* </Listbox.Option>
-                                        ))} */}
-                                    </Listbox.Options>
-                                </Transition>
+                        <button onClick={handleCategory}>
+                            <div className='w-[200px] ml-3 h-full flex items-center justify-between '>
+                                <p>{selected ? selected : 'Kategori'}</p>
+                                <img src={arrowDown} alt="" />
                             </div>
-                        </Listbox>
+                        </button>
+                        {isClicked ?
+
+                            <div className='absolute mt-2 h-[200px] overflow-auto z-10 top-full w-[200px] border-2 border-black bg-white'>
+                                <ul className='flex flex-col '>
+                                    {category.map((item, index) => (
+                                        <button className='' key={index} value={item.name} onClick={() => toggleSelected(item.name)}>
+                                            <li className='hover:bg-blue-300  flex items-center justify-center h-[50px] '>{item.name}</li>
+                                        </button>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            : null}
+
 
                         <div className='h-full w-1 rounded-full bg-black'></div>
 
                         <div className=' w-full'>
                             <input className='w-full h-full px-3' type="search" placeholder='Search' />
                         </div>
+                        <button className=' w-[100px] h-full  flex justify-center'>
+                            <img className=' w-[40px]' src={searchLogo} alt="" />
+                        </button>
                     </div>
                     <button>
                         <img src={Heart} alt="Wishlist" />
@@ -156,11 +107,24 @@ export default function Header() {
                     <button>
                         <img src={Bell} alt="Notification" />
                     </button>
-                    <div className='flex items-center gap-x-4'>
+                    <div className='relative flex items-center gap-x-4'>
                         <img src={UserLogo} alt="User" />
-                        <button>
-                            <p className='text-white font-[400]'>User</p>
+                        <button onClick={() => setIsClickedLogin(!isClickedLogin)}>
+                            <p className='text-white font-[400]'>Masuk/Daftar</p>
                         </button>
+
+                        {isClickedLogin ?
+                            <div className='bg-white absolute border-2 border-black w-full top-full mt-5 flex flex-col'>
+                                <button >
+                                    <div className='h-[40px] flex justify-center items-center'>Masuk</div>
+                                </button>
+                                <button >
+                                    <div className='h-[40px] flex justify-center items-center'>Daftar</div>
+                                </button>
+                            </div>
+
+                            : null}
+
                     </div>
                 </div>
             </nav>
