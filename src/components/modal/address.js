c4b74d2c34c889daac4f2b9d2f3b2818b3ae906a
adithiarisma <adithiarisma@gmail.com>
2022-06-17 18:12:41 +0700

    responsive dan connect backend

--- src/components/modal/address.js
+++ src/components/modal/address.js
@@ -5,22 +5,13 @@ import { addressContext } from '../../context/context'
 
 function Address({ setAddress, setAlamatPengguna, setAlamat, alamatPengguna }) {
 
-    const [userAddress, setUserAddress] = React.useState({
-        labelAlamat: "",
-        provinsi: "Aceh",
-        kabupaten: "",
-        kecamatan: "",
-        kelurahan: "",
-        kodePos: "",
-        namaLengkap: "",
-        noHp: "",
-        alamatUtama: false
-    })
+    const [userAddress, setUserAddress] = React.useState({})
 
     const addressHandler = (e) => {
         setUserAddress(prevData => {
             return {
                 ...prevData,
+                id: Math.trunc(Date.now() + Math.random()),
                 [e.target.name]: e.target.value
             }
         })
@@ -35,12 +26,25 @@ function Address({ setAddress, setAlamatPengguna, setAlamat, alamatPengguna }) {
         })
     }
 
-    console.log(userAddress)
 
     const changeAddress = () => {
-        setAlamatPengguna(userAddress)
+        // setAlamatPengguna(prevState => {
+        //     return {
+        //         ...prevState,
+        //         userAddress
+        //     }
+        // })
+        setAlamatPengguna(prevState => {
+            return [
+                ...prevState,
+                {
+                    userAddress
+                }
+            ]
+        })
         setAlamat(true)
         setAddress(false)
+        setUserAddress({})
     }
 
     const { listProvinsi } = useContext(addressContext)
@@ -48,13 +52,17 @@ function Address({ setAddress, setAlamatPengguna, setAlamat, alamatPengguna }) {
 
 
     return (
-        <div className='modal-bg flex w-full h-full absolute bg-red-500 left-0 top-0 justify-center items-center'>
-            <div className='relative modal-auto flex flex-col items-center justify-center w-[80%] md:w-[40%] h-[90%] rounded-md bg-white p-16 z-99'>
-                <img onClick={() => setAddress(false)} className="cursor-pointer absolute right-[20px] top-[20px] max-w-[20px]" src={closeIcon} alt="" />
+        <>
+            <div className='modal-bg flex w-full absolute left-0 right-0 bottom-0 top-0 justify-center items-center'>
+            </div>
+            <div className='absolute mx-auto overflow-y-auto h-[90%] left-[50%] translate-xy top-[50%] w-[50%] flex flex-col items-center rounded-md bg-white p-5 z-99'>
+                <div className='w-full'>
+                    <img onClick={() => setAddress(false)} className="cursor-pointer max-w-[10px] sm:max-w-[20px] float-right" src={closeIcon} alt="" />
+                </div>
                 <h1 className='py-3 text-center font-bold text-[#316093]'>Tambahkan Alamat</h1>
                 <h1 className='py-2 w-full'>Informasi Detail Alamat</h1>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input name="labelAlamat" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Label Alamat' />
+                    <input required name="labelAlamat" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Label Alamat' />
                 </div>
                 <select name="provinsi" onChange={(e) => addressHandler(e)} className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
                     {listProvinsi.provinsi.map(each => {
@@ -64,30 +72,33 @@ function Address({ setAddress, setAlamatPengguna, setAlamat, alamatPengguna }) {
                     })}
                 </select>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input name="kabupaten" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kabupaten / Kota' />
+                    <input required name="kabupaten" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kabupaten / Kota' />
                 </div>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input name="kecamatan" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kecamatan' />
+                    <input required name="kecamatan" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kecamatan' />
                 </div>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input name="kelurahan" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kelurahan' />
+                    <input required name="kelurahan" onChange={(e) => addressHandler(e)} className='w-full' placeholder='Kelurahan' />
                 </div>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input onChange={(e) => addressHandler(e)} name="kodePos" className='w-full' placeholder='Kode Pos' />
+                    <input required onChange={(e) => addressHandler(e)} name="kodePos" className='w-full' placeholder='Kode Pos' />
+                </div>
+                <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
+                    <input required onChange={(e) => addressHandler(e)} name="alamatLengkap" className='w-full' placeholder='Alamat Lengkap' />
                 </div>
                 <h1 className='py-2 mt-4 w-full'>Informasi Penerima</h1>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input onChange={(e) => addressHandler(e)} name="namaLengkap" className='w-full' placeholder='Nama Lengkap Penerima' />
+                    <input required onChange={(e) => addressHandler(e)} name="namaLengkap" className='w-full' placeholder='Nama Lengkap Penerima' />
                 </div>
                 <div className='p-2 mt-6 w-full rounded-md border-[1px] border-[#316093]'>
-                    <input onChange={(e) => addressHandler(e)} name="noHp" className='w-full' placeholder='No Hp Penerima' />
+                    <input required onChange={(e) => addressHandler(e)} name="noHp" className='w-full' placeholder='No Hp Penerima' />
                 </div>
                 <div className='w-full mt-4'>
-                    <input onChange={() => mainAddress()} id="utama" type="checkbox" /> <label htmlFor="utama">Jadikan Alamat Utama</label>
+                    <input required onChange={() => mainAddress()} id="utama" type="checkbox" /> <label htmlFor="utama">Jadikan Alamat Utama</label>
                 </div>
                 <button onClick={() => changeAddress()} className='p-2 font-semibold px-12 rounded-md mt-6 bg-[#FBC646]'>Simpan Alamat</button>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AddressProvider from "./context/context.js";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AddressProvider>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </AddressProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
