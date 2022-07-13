2d6094a98d235ca60029c9d371c114911a161315
mfaishaladiyatma <mfaishall.adiyatma@gmail.com>
2022-07-13 21:32:07 +0700

    search Wishlist

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import jwtDecode from 'jwt-decode'
 import { useSelector } from 'react-redux/es/hooks/useSelector'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, useSearchParams } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast';
 
 import gambarWishlist from '../images/gambarWishlist.svg'
@@ -14,10 +14,20 @@ import trashIcon from '../images/trashIcon.svg'
 
 export default function Wishlist() {
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const navigate = useNavigate()
 
   const [dataWishlist, setDataWishlist] = useState([])
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  const [filterList, setFilterList] = useState({
+    name: '',
+    page: 0,
+    size: 12
+  });
 
   const { token } = useSelector((state) => state)
   const idAkun = useSelector((state) => state.id)
@@ -26,29 +36,70 @@ export default function Wishlist() {
 
   const decode = token ? jwtDecode(token) : null
 
-  console.log(dataWishlist ? dataWishlist : "no data")
 
   useEffect(() => {
-    if (decode) {
-      setLoading(true)
-      axios.get('https://homepoint-server-staging.herokuapp.com/api/v1/wishlist/' + id)
-        .then((response) => {
-          // setDataWishlist((prevState) =>([
-          //   ...prevState,
-          //   response.data.data
-          // ]))
-          setDataWishlist(response.data.data.wishlistItems)
-          setLoading(false)
-          // console.log(cobaGet)
-        })
-        .catch((error) => {
-          console.log(error)
-          setLoading(false)
-        })
-    } else {
-      return
+    const fetchData = async () => {
+      if (decode) {
+        setLoading(true)
+        const getWishlistItems = await axios.get('https://homepoint-server-staging.herokuapp.com/api/v1/wishlist/' + id)
+          .then((response) => {
+            // setDataWishlist((prevState) =>([
+            //   ...prevState,
+            //   response.data.data
+            // ]))
+            setDataWishlist(response.data.data.wishlistItems)
+            setLoading(false)
+            // console.log(cobaGet)
+          })
+          .catch((error) => {
+            console.log(error)
+            setLoading(false)
+          })
+      } else {
+        return
+      }
     }
+
+    fetchData()
+
   }, [])
+
+  // const name = searchParams.get('name');
+  // const page = searchParams.get('page');
+  // const size = searchParams.get('size');
+
+  // const url = require("url")
+
+  const queryParams = {
+    productName: search,
+    page: 0,
+    size: 12,
+  }
+
+  const params = new URLSearchParams(queryParams)
+
+  const searchHandler = (e) => {
+    e.preventDefault()
+    // setFilterList((prevState) => ({
+    //   ...prevState,
+    //   name:search,
+    //   page,
+    //   size,
+    // }));
+
+    axios({
+      method: 'GET',
+      url: 'https://homepoint-server-staging.herokuapp.com/api/v1/wishlist/' + idAkun,
+      params
+    })
+      .then((response) => {
+        setDataWishlist(response.data.data.wishlistItems)
+      })
+      .catch((error) => {
+        console.log(error)
+        //wip: display error here
+      });
+  }
 
   const getWishlistData = () => {
     axios.get('https://homepoint-server-staging.herokuapp.com/api/v1/wishlist/' + id)
@@ -140,11 +191,11 @@ export default function Wishlist() {
 
             <div className='flex flex-row  p-3 w-[700px] justify-between'>
               <h3 className='text-[32px] font-bold'>Wishlist</h3>
-              <div className='border-2 rounded-[8px] border-[#316093] w-[450px] flex items-center px-3'>
-                <form >
-                  <input placeholder='Search Wishlist' type="text" />
-                </form>
-              </div>
+              <form onSubmit={searchHandler}>
+                <div className='border-2 rounded-[8px] border-[#316093] w-[450px] flex items-center p-3'>
+                  <input className='w-full' onChange={(e) => setSearch(e.target.value)} value={search} placeholder='Search Wishlist' type="text" />
+                </div>
+              </form>
             </div>
             <div>
               <p className='text-[#316093]'>
@@ -158,42 +209,43 @@ export default function Wishlist() {
 
           <div className=' mt-10'>
             <div className='grid grid-cols-6 h-fit gap-3 p-3'>
+              
+                {dataWishlist.length > 0 && dataWishlist.map((data) => {
+                  // console.log(data.wishlistItems)
+                  return (
 
-              {dataWishlist.map((data) => {
-                // console.log(data.wishlistItems)
-                return (
+                    <div key={data.id} className='border-2 border-slate-300 flex flex-col items-center justify-between rounded-[8px] p-2 gap-y-5 max-h-[450px]'>
+                      <img src={data.products.productImages[0].image} alt="" />
 
-                  <div key={data.id} className='border-2 border-slate-300 flex flex-col items-center justify-between rounded-[8px] p-2 gap-y-5 max-h-[450px]'>
-                    <img src={data.products.productImages[0].image} alt="" />
+                      <h3 className='w-full text-left font-bold text-[18px]'>{data.products.name}</h3>
 
-                    <h3 className='w-full text-left font-bold text-[18px]'>{data.products.name}</h3>
-
-                    <div className='flex flex-col w-full gap-y-2'>
-                      <h4 className='text-[14px] text-gray-500 line-through decoration-red-600 decoration-2 text-left'>{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 6 }).format(data.products.price * (data.products.discount / 100))}</h4>
-                      <h3 className='font-bold text-[18px] text-left'>{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 6 }).format(data.products.price)}</h3>
-                      <div className='flex flex-row items-center gap-x-2'>
-                        <div className='text-yellow-500'>
-                          <AiFillStar />
+                      <div className='flex flex-col w-full gap-y-2'>
+                        <h4 className='text-[14px] text-gray-500 line-through decoration-red-600 decoration-2 text-left'>{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 6 }).format(data.products.price * (data.products.discount / 100))}</h4>
+                        <h3 className='font-bold text-[18px] text-left'>{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 6 }).format(data.products.price)}</h3>
+                        <div className='flex flex-row items-center gap-x-2'>
+                          <div className='text-yellow-500'>
+                            <AiFillStar />
+                          </div>
+                          <p>{data.products.ratingAverage}</p>
+                          <p> | </p>
+                          <p>Terjual {data.products.amountSold}</p>
                         </div>
-                        <p>{data.products.ratingAverage}</p>
-                        <p> | </p>
-                        <p>Terjual {data.products.amountSold}</p>
+                        <div className='flex flex-row gap-x-3'>
+                          <button onClick={() => deleteWishlist(data.id)}>
+                            <img className='w-[25px]' src={trashIcon} alt="" />
+                          </button>
+                          <button onClick={() => addToCart(data.products.id)} className='bg-[#FBC646] p-3 flex flex-row justify-between w-[130px] font-semibold rounded-[8px] text-[#252525]'>
+                            <p>+</p>
+                            <p>Keranjang</p>
+                          </button>
+                        </div>
                       </div>
-                      <div className='flex flex-row gap-x-3'>
-                        <button onClick={() => deleteWishlist(data.id)}>
-                          <img className='w-[25px]' src={trashIcon} alt="" />
-                        </button>
-                        <button onClick={() => addToCart(data.products.id)} className='bg-[#FBC646] p-3 flex flex-row justify-between w-[130px] font-semibold rounded-[8px] text-[#252525]'>
-                          <p>+</p>
-                          <p>Keranjang</p>
-                        </button>
-                      </div>
-                    </div>
 
-                  </div>
-                )
-              }
-              )}
+                    </div>
+                  )
+                }
+                )}
+              
 
             </div>
           </div>
